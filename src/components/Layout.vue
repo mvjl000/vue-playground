20c1fb06d40778e54bc5033e706864ec6c914980
mvjl000 <milosz.devmail@gmail.com>
2022-04-20 21:26:45 +0200

    Initial setup

---
+++ src/components/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="layout">hello world</div>
+</template>
+
+<script setup lang="ts">
+console.log('Hello?');
+</script>
+
+<style lang="scss">
+.layout {
+  color: red;
+  p {
+    color: blue;
+  }
+}
+</style>
