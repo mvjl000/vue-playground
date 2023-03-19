0776808b07028d7852972d9854ee61b3e521e7b7
Miłosz <milosz.devmail@gmail.com>
2023-03-19 21:58:57 +0100

    Change colors

--- src/components/pages/Tabs.vue
+++ src/components/pages/Tabs.vue
@@ -45,13 +45,13 @@ const closeTab = (tabId: number) => {
   <h1 class="text-white text-4xl text-center tracking-wide">Tabs</h1>
   <div class="mt-8 w-full flex-grow flex">
     <div class="h-full min-w-[100px] w-1/6 bg-neutral-800"></div>
-    <div class="flex flex-col h-full flex-grow bg-neutral-900">
+    <div class="flex flex-col h-full flex-grow bg-stone-900">
       <ul class="h-14 flex justify-start items-end gap-2 bg-neutral-800">
         <li
           v-for="tab in tabs"
           :key="tab.id"
           class="group h-2/3 pl-3 pr-1 flex justify-between items-center gap-1 text-white bg-stone-700 rounded-tl-md rounded-tr-md cursor-pointer"
-          :class="{ 'bg-neutral-900': activeTab === tab.id }"
+          :class="{ 'bg-stone-900': activeTab === tab.id }"
           @click="setAcitveTab(tab.id)"
         >
           {{ tab.name }}
